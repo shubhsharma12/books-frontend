7c891d9ca06574b1d61db799b42a71987e590395
Shubhankar Sharma <sharmashubhankar19@gmail.com>
2025-02-16 23:55:37 +0530

    enabled routing in angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },  // Default route
+  { path: '**', redirectTo: '/login' }  // Wildcard route for invalid paths
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
